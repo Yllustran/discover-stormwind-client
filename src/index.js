0870e23d6b5948c9baefccd60baff95e0196613a
Yllustran <sullivantra@gmail.com>
2025-02-05 11:35:14 +0100

    feat(map): install Leaflet and add required styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  './assets/styles/Main.css';
+import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
